200f514f7f30236a00132f07e8453a5930c682d7
Christof Laenzlinger <christof.laenzlinger@swisscom.com>
2019-12-27 00:18:25 +0100

    add user setup script

---
+++ arch/setup-user.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+sudo pacman -S --needed - < $USER/.config/pacman/pkglist.txt
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+
+# Vim Plug (afterwards run :PlugInsall in nvim)
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+nvim +'PlugInstall --sync' +qa
