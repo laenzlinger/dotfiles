b6d6f6db8c889c8e038e1dc4a757bbbc2df5dbe6
Christof Laenzlinger <christof.laenzlinger@swisscom.com>
2019-12-26 23:43:25 +0100

    Fix locale.gen

--- arch/setup.sh
+++ arch/setup.sh
@@ -53,8 +53,8 @@ echo "Set Time Zone"
 ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
 
 echo "Set Locale"
-echo "en_US.UTF-8" >> /etc/locale.gen
-echo "de_CH.UTF-8" >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "de_CH.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
