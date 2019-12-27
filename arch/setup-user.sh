35167a294178d9c4bc7c91184b75087a77f65a71
Christof Laenzlinger <christof.laenzlinger@swisscom.com>
2019-12-27 14:17:11 +0100

    get rid of i3 style - instead introduce template

--- arch/setup-user.sh
+++ arch/setup-user.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-
+# install packages
 sudo pacman -S --needed - < $HOME/.local/share/chezmoi/dot_config/pacman/pkglist.txt
+
+# install aur packages
+mkdir -p $HOME/src/aur
+pushd $HOME/src/aur
+git clone https://aur.archlinux.org/nerd-fonts-meslo.git
+cd nerd-fonts-meslo
+makepkg -si --noconfirm
+popd
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -9,20 +17,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 
-# Vim Plug (afterwards run :PlugInsall in nvim)
+# Vim Plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+chezmoi apply
 
-mkdir -p $HOME/src/aur
-pushd $HOME/src/aur
-git clone https://aur.archlinux.org/j4-make-config-git.git
-cd j4-make-config-git
-makepkg -si --noconfirm
-cd $HOME/src/aur
-git clone https://aur.archlinux.org/nerd-fonts-meslo.git
-cd nerd-fonts-meslo
-makepkg -si --noconfirm
-popd
-
-echo nvim +'PlugInstall --sync' +qa
+nvim +'PlugInstall --sync' +qa
--- dot_config/i3/run_chezmoi_apply.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -f $HOME/.config/i3/config.base ]]; then
-    j4-make-config base16-tomorrow
-fi
