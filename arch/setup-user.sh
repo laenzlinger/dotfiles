4c795809032f89a7bd117baf8dddd449ce21c29c
Christof Laenzlinger <christof.laenzlinger@swisscom.com>
2020-03-27 08:17:09 +0100

    install google-cloud-sdk

--- arch/setup-user.sh
+++ arch/setup-user.sh
@@ -15,6 +15,7 @@ sudo pacman -S --needed - < ~/.local/share/chezmoi/dot_config/pacman/pkglist.txt
 # install aur packages
 install-aur nerd-fonts-meslo
 install-aur cloudfoundry-cli
+install-aur google-cloud-sdk
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
