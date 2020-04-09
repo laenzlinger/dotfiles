2414d87dd6d830a00a50f65475a2e196224d29fd
Christof Laenzlinger <laenzlinger@github.com>
2020-04-09 17:22:18 +0200

    write the changs to the installed package to the chezmoi source files instead of target

--- dot_config/pacman/run_chezmoi_apply.sh
+++ dot_config/pacman/run_chezmoi_apply.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR=~/.local/share/chezmoi/dot_config/pacman
 
 pacman -Qqen > $DIR/pkglist.txt
 pacman -Qqem > $DIR/foreignpkglist.txt
