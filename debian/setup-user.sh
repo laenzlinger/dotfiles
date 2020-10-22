8b819ace65185e7332f57f8258879b50061edb0a
Christof Laenzlinger <laenzlinger@github.com>
2020-10-22 21:26:42 +0200

    dont use nvim on debian

--- debian/setup-user.sh
+++ debian/setup-user.sh
@@ -9,10 +9,10 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/cu
 # Base 16 shell
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
-# Vim Plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# No Vim Plug currently on debian
+#curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ~/bin/chezmoi apply
 
-nvim +'PlugInstall --sync' +qa
+# nvim +'PlugInstall --sync' +qa
