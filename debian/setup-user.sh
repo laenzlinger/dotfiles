3b20de124922229243bd1e7ffdeefbd27dab93e8
Christof Laenzlinger <laenzlinger@github.com>
2020-10-22 21:42:18 +0200

    unattended oh-my-zsh install on debian

--- debian/setup-user.sh
+++ debian/setup-user.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
