b4df740138adfe95bca19f094bf7aa12073bc79c
Christof Laenzlinger <laenzlinger@github.com>
2020-10-23 10:27:51 +0000

    fix debian installation

--- debian/setup-user.sh
+++ debian/setup-user.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+git config --global http.proxy $https_proxy
+
 # oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 
 # powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+~/.oh-my-zsh/custom/themes/powerlevel10k/gitstatus/install -f
 
 # Base 16 shell
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
