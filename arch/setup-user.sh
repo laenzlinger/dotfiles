29604157bac740180a6157d55210592400f75da7
Christof Laenzlinger <laenzlinger@github.com>
2020-11-06 23:54:28 +0100

    install vimplug with vi script

--- arch/setup-user.sh
+++ arch/setup-user.sh
@@ -19,17 +19,13 @@ install-aur google-cloud-sdk
 install-aur velero-bin
 
 # oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 
 # powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
 # Base 16 shell
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-
-# Vim Plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 chezmoi apply
 
--- darwin/setup-user.sh
+++ darwin/setup-user.sh
@@ -9,17 +9,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 brew bundle --file=$DIR/../dot_config/homebrew/Brewfile
 
 # oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 
 # powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 
 # Base 16 shell
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-
-# Vim Plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 chezmoi apply
 
