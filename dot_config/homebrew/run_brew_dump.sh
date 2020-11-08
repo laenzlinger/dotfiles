28a5ae51cb7e1388dffa69435ccbeee724fee22b
Christof Laenzlinger <laenzlinger@github.com>
2020-11-08 13:54:35 +0100

    export Brewfile to chezmoi source by default

--- dot_config/homebrew/run_brew_dump.sh
+++ dot_config/homebrew/run_brew_dump.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR=~/.local/share/chezmoi/dot_config/homebrew
 
 cd $DIR
-brew bundle dump --force
+brew bundle dump --force --no-upgrade
