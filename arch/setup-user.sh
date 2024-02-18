9aaac96d08b9465dd40dd08ca17c5adbd15a6504
Christof <6319634+laenzlinger@users.noreply.github.com>
2024-02-18 15:16:46 +0100

    Improve yay installation

--- arch/setup-user.sh
+++ arch/setup-user.sh
@@ -5,9 +5,11 @@ sudo pacman -S --needed - < ~/.local/share/chezmoi/dot_config/pacman/pkglist.txt
 
 # install yay
 
+cd ~
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+rm -rf ~/yay
 
 
 chezmoi apply
