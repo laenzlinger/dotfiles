40fcbbb60e15a52e46dc8c56509a2ec0b81a3d7c
Christof <6319634+laenzlinger@users.noreply.github.com>
2024-02-18 15:22:27 +0100

    no confirm on package installation

--- arch/setup-user.sh
+++ arch/setup-user.sh
@@ -3,10 +3,9 @@
 set -e
 
 # install packages
-sudo pacman -S --needed - < ~/.local/share/chezmoi/dot_config/pacman/pkglist.txt
+sudo pacman -S --noconfirm --needed - < ~/.local/share/chezmoi/dot_config/pacman/pkglist.txt
 
 # install yay
-
 cd ~
 git clone https://aur.archlinux.org/yay.git
 cd yay
