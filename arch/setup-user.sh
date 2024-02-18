2c8cbd2c629bde3e3b1281b183aabc0d443bea73
Christof <6319634+laenzlinger@users.noreply.github.com>
2024-02-18 12:45:02 +0100

    Improve setupuers

--- arch/setup-user.sh
+++ arch/setup-user.sh
@@ -14,18 +14,12 @@ sudo pacman -S --needed - < ~/.local/share/chezmoi/dot_config/pacman/pkglist.txt
 
 # install aur packages
 install-aur nerd-fonts-meslo
-install-aur cloudfoundry-cli
-install-aur google-cloud-sdk
-install-aur velero-bin
 
 # oh-my-zsh
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 
 # powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
-
-# Base 16 shell
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
 chezmoi apply
 
