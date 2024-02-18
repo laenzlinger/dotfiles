7e0734919023f0f4f774ce58e8d4aebb74c84f9c
Christof <6319634+laenzlinger@users.noreply.github.com>
2024-02-18 13:54:02 +0100

    Move more of the installation to chezmoi

--- arch/setup-user.sh
+++ arch/setup-user.sh
@@ -3,5 +3,7 @@
 # install packages
 sudo pacman -S --needed - < ~/.local/share/chezmoi/dot_config/pacman/pkglist.txt
 
+echo "setup yay"
+
 chezmoi apply
 
--- darwin/setup-user.sh
+++ darwin/setup-user.sh
@@ -8,14 +8,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # install packages
 brew bundle --file=$DIR/../dot_config/homebrew/Brewfile
 
-# oh-my-zsh
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
-
-# powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
-
-# Base 16 shell
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-
 chezmoi apply
 
