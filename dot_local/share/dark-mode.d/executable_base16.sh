bb369bdd7c78551222afc858c9435a39d9ca28fe
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2024-04-27 17:47:12 +0200

    Use darkman to switch base16-shell

---
+++ dot_local/share/dark-mode.d/executable_base16.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @file        : base16
+# @created     : Saturday Apr 27, 2024 16:32:42 CEST
+#
+# @description : set base16 to dark mode (called by darkman)
+######################################################################
+
+BASE16_SHELL="$HOME/.config/base16-shell"
+
+source "$BASE16_SHELL/profile_helper.sh"
+
+base16_darktooth
+#_base16 "$BASE16_SHELL/scripts/base16-darktooth.sh" darktooth
+
---
+++ dot_local/share/light-mode.d/executable_base16.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @file        : base16
+# @created     : Saturday Apr 27, 2024 16:30:30 CEST
+#
+# @description : set base16 shell to light mode (called by darkman)
+######################################################################
+
+
+BASE16_SHELL="$HOME/.config/base16-shell"
+
+source "$BASE16_SHELL/profile_helper.sh"
+
+base16_unikitty-light
+#
+
+
