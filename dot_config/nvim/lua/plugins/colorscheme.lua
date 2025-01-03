399cc4f2a619af1715f0c69cf2f8ac242cb5af66
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2025-01-03 17:08:12 +0100

    Add a hook for tinty

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,25 @@
+local function handle_focus_gained()
+  local new_theme_name = vim.fn.system("tinty current")
+  local current_theme_name = vim.g.colors_name
+
+  if current_theme_name ~= new_theme_name then
+    vim.cmd("colorscheme " .. new_theme_name)
+  end
+end
+
 return {
   {
     "tinted-theming/tinted-vim",
     lazy = false,
     priority = 1000,
     config = function()
-      local theme_name = vim.fn.system("tinty current")
       vim.g.tinted_background_transparent = 1
-      vim.cmd("colorscheme " .. theme_name)
+
+      local new_theme_name = vim.fn.system("tinty current")
+      vim.cmd("colorscheme " .. new_theme_name)
+      vim.api.nvim_create_autocmd("FocusGained", {
+        callback = handle_focus_gained,
+      })
     end,
   },
   {
