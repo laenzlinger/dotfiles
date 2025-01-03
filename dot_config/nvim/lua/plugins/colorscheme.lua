346a932fe6c7947159cb26cb298855a4fa01fd84
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2025-01-03 16:46:28 +0100

    Update to latest tinted theming

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
   {
-    "tinted-theming/base16-vim",
+    "tinted-theming/tinted-vim",
     lazy = false,
     priority = 1000,
     config = function()
       local theme_name = vim.fn.system("tinty current")
-      vim.g.base16_background_transparent = 1
+      vim.g.tinted_background_transparent = 1
       vim.cmd("colorscheme " .. theme_name)
     end,
   },
@@ -14,5 +14,12 @@ return {
     opts = {
       colorscheme = function() end,
     },
+  },
+  -- once the lualine fork is merged, we can remove this
+  -- https://github.com/JamyGolden/lualine.nvim.git
+  -- ~home/projects can then also be removed
+  {
+    "nvim-lualine/lualine.nvim",
+    dev = true,
   },
 }
