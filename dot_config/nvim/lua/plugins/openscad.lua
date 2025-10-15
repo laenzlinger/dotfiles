44217eb378bde1daf20f8394a81fec89eeee08e6
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2025-10-15 12:41:47 +0200

    Fix mason config

--- dot_config/nvim/lua/plugins/openscad.lua
+++ dot_config/nvim/lua/plugins/openscad.lua
@@ -8,11 +8,19 @@ return {
     },
   },
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
         "openscad-lsp",
       },
     },
+  },
+  {
+    "salkin-mada/openscad.nvim",
+    config = function()
+      vim.g.openscad_load_snippets = true
+      require("openscad")
+    end,
+    dependencies = { "L3MON4D3/LuaSnip", "junegunn/fzf.vim" },
   },
 }
