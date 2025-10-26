5484aa582c092bf387947dded57e76055679d410
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2025-10-26 23:02:31 +0100

    Improve starting of keepassxc and show wob on mute

--- dot_config/sway/bin/executable_volume.sh
+++ dot_config/sway/bin/executable_volume.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -Eo '[+-]?[0-9]+([.][0-9]+)?' | awk '{print "scale=0;"$1"*10000/100"}' | bc
+wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -Eo '[+-]?[0-9]+([.][0-9]+)?' | awk '{print "scale=0;"$1"*10000/100"}' | bc | tr '\n' ' '
+wpctl get-volume @DEFAULT_AUDIO_SINK@ | if ! grep -Eo MUTED; then echo ""; fi
