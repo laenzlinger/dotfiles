55ece7f0ea03c61ed6ab4f83045ba4ee1e7aeb0a
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2025-11-10 23:52:13 +0100

    improve darkman config

---
+++ dot_local/share/darkman/executable_desktop-notification.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# trigger a small, passive popup dialog to inform the user about darkman's activity
+# reference https://wiki.archlinux.org/title/Desktop_notifications#Usage_in_programming
+
+case "$1" in
+dark) ICON=weather-clear-night ;;
+light) ICON=weather-clear ;;
+esac
+
+notify-send --app-name="darkman" --urgency=low --icon="$ICON" "switching to $1 mode"
---
+++ dot_local/share/darkman/executable_keyboard-backlight.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Control keyboard backlight based on mode. Should be vendor agnostic.
+# reference https://wiki.archlinux.org/title/Keyboard_backlight#D-Bus
+
+# Check the max brightness value for your hardware with:
+# dbus-send --type=method_call --print-reply=literal --system --dest="org.freedesktop.UPower" /org/freedesktop/UPower/KbdBacklight org.freedesktop.UPower.KbdBacklight.GetMaxBrightness
+
+case "$1" in
+dark) BRIGHTNESS=2 ;;  # Activate keyboard backlight in dark mode
+light) BRIGHTNESS=0 ;; # Disable keyboard backlight in light mode
+esac
+
+dbus-send --system --type=method_call --dest="org.freedesktop.UPower" "/org/freedesktop/UPower/KbdBacklight" "org.freedesktop.UPower.KbdBacklight.SetBrightness" "int32:$BRIGHTNESS"
