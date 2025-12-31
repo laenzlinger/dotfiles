add865d31f6ff4ae748cfd749ea6462ae6452b70
Christof <6319634+laenzlinger@users.noreply.github.com>
2025-12-31 17:39:28 +0100

    Change wezterm font size on apple

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,12 +1,15 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
-
 config.font_size = 10
 config.font = wezterm.font("MesloLGS Nerd Font")
 config.color_scheme = "tinted-theming"
 config.window_background_opacity = 0.9
 config.text_background_opacity = 0.9
 config.hide_tab_bar_if_only_one_tab = true
+
+if wezterm.target_triple:find("apple") then
+	config.font_size = 14
+end
 
 return config
