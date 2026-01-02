cd8491176fc0bd57c06283f4deca075aaf716f92
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2026-01-02 13:52:18 +0100

    Fix colors in bat

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -8,6 +8,14 @@ config.window_background_opacity = 0.9
 config.text_background_opacity = 0.9
 config.hide_tab_bar_if_only_one_tab = true
 
+config.mouse_bindings = {
+	{
+		event = { Down = { streak = 3, button = "Left" } },
+		action = wezterm.action.SelectTextAtMouseCursor("SemanticZone"),
+		mods = "NONE",
+	},
+}
+
 if wezterm.target_triple:find("apple") then
 	config.font_size = 14
 end
