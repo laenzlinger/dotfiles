fc4a82f95749e9656c7d45169c6687cfb191ab9b
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2026-01-10 18:18:15 +0100

    move waybar to the main screen

--- dot_config/sway/bin/executable_move-workspace-to-output.sh
+++ dot_config/sway/bin/executable_move-workspace-to-output.sh
@@ -2,11 +2,18 @@
 
 OUTPUT=$1
 
+# workspace
 swaymsg [workspace="1"] move workspace to output "$OUTPUT"
 swaymsg [workspace="2"] move workspace to output "$OUTPUT"
 swaymsg [workspace="3"] move workspace to output "$OUTPUT"
 swaymsg [workspace="4"] move workspace to output "$OUTPUT"
 swaymsg [workspace="5"] move workspace to output "$OUTPUT"
 
+# waybar
+CONFIG=~/.config/waybar/config.jsonc
+sed -i "s/\"output\": \".*\"/\"output\": \"$OUTPUT\"/" "$CONFIG"
+killall waybar && waybar &
+
+# notification
 swaync-client --change-cc-monitor "$OUTPUT"
 swaync-client --change-noti-monitor "$OUTPUT"
