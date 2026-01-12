41ff7c730ad4a75a8aaadb08cb8b854c938d84db
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2026-01-12 20:03:20 +0100

    Add workaround for icons not showing up correctly

--- dot_local/share/darkman/executable_desktop-notification.sh
+++ dot_local/share/darkman/executable_desktop-notification.sh
@@ -2,10 +2,13 @@
 
 # trigger a small, passive popup dialog to inform the user about darkman's activity
 # reference https://wiki.archlinux.org/title/Desktop_notifications#Usage_in_programming
+#
+
+ICON_WORKAROUND="/usr/share/icons/Papirus/48x48/status"
 
 case "$1" in
-dark) ICON=weather-clear-night ;;
-light) ICON=weather-clear ;;
+dark) ICON=$ICON_WORKAROUND/weather-clear-night.svg ;;
+light) ICON=$ICON_WORKAROUND/weather-clear.svg ;;
 esac
 
-notify-send --app-name="darkman" --urgency=low --icon="$ICON" "switching to $1 mode"
+notify-send --app-name="darkman" --urgency=low --icon="$ICON" "darkman" "switching to $1 mode"
