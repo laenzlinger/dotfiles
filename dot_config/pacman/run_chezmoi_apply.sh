ade5636274644dedbd1c1e7b13860f84c1c950f8
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2026-02-01 20:09:06 +0100

    Backup the status of the systemd units

--- dot_config/pacman/run_chezmoi_apply.sh
+++ dot_config/pacman/run_chezmoi_apply.sh
@@ -3,7 +3,11 @@
 # DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DIR=~/.local/share/chezmoi/dot_config/pacman
 
-pacman -Qqen > $DIR/pkglist.txt
-pacman -Qqem > $DIR/foreignpkglist.txt
-echo "#optional dependencies" > $DIR/optdeplist.txt
-comm -13 <(pacman -Qqdt | sort) <(pacman -Qqdtt | sort) >> $DIR/optdeplist.txt
+pacman -Qqen >$DIR/pkglist.txt
+pacman -Qqem >$DIR/foreignpkglist.txt
+echo "#optional dependencies" >$DIR/optdeplist.txt
+comm -13 <(pacman -Qqdt | sort) <(pacman -Qqdtt | sort) >>$DIR/optdeplist.txt
+
+# systemd services
+systemctl list-unit-files --state=enabled --no-legend | awk '{print $1}' >$DIR/enabled_units.txt
+systemctl --user list-unit-files --state=enabled --no-legend | awk '{print $1}' >$DIR/enabled_user_units.txt
