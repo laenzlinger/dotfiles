48c3f04d010a6811256eead0ab471ddc9e325022
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2026-03-08 23:24:28 +0100

    fix: sway reload on theme switch via darkman/tinty

--- dot_local/share/darkman/executable_tinty.sh
+++ dot_local/share/darkman/executable_tinty.sh
@@ -6,4 +6,6 @@ dark) THEME=base16-darktooth ;;
 light) THEME=base16-solarized-light ;;
 esac
 
+SWAYSOCK=$(find /run/user/"$(id -u)" -name 'sway-ipc.*.sock' 2>/dev/null | head -1)
+export SWAYSOCK
 /usr/bin/tinty apply "$THEME"
