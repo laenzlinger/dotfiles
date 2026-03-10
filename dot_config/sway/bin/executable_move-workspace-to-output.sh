258613c742467788c6b49e8839217337a77be817
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2026-03-10 15:53:25 +0100

    fix: only move existing workspaces to output

--- dot_config/sway/bin/executable_move-workspace-to-output.sh
+++ dot_config/sway/bin/executable_move-workspace-to-output.sh
@@ -4,8 +4,8 @@ OUTPUT="$1"
 
 swaymsg focus output "$OUTPUT"
 
-for ws in $(seq 1 9); do
-    swaymsg "[workspace=\"$ws\"] move workspace to output \"$OUTPUT\"" || true
+for ws in $(swaymsg -t get_workspaces | jq -r '.[].name'); do
+    swaymsg "[workspace=\"$ws\"] move workspace to output \"$OUTPUT\""
 done
 
 CONFIG=~/.config/waybar/config.jsonc
