a5ea2605ffd028a038933404f8eb49901e54a3cf
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2026-03-10 16:03:10 +0100

    fix: use proper sway workspace command syntax for output moves

--- dot_config/sway/bin/executable_move-workspace-to-output.sh
+++ dot_config/sway/bin/executable_move-workspace-to-output.sh
@@ -5,12 +5,14 @@ OUTPUT="$1"
 swaymsg focus output "$OUTPUT"
 
 for ws in $(swaymsg -t get_workspaces | jq -r '.[].name'); do
-    swaymsg "[workspace=\"$ws\"] move workspace to output \"$OUTPUT\""
+    swaymsg "workspace $ws, move workspace to output $OUTPUT"
 done
 
 CONFIG=~/.config/waybar/config.jsonc
 sed -i "s/\"output\": \".*\"/\"output\": \"$OUTPUT\"/" "$CONFIG"
-killall -SIGUSR2 waybar
+killall -q waybar || true
+sleep 0.3
+swaymsg exec waybar
 
 swaync-client --change-cc-monitor "$OUTPUT"
 swaync-client --change-noti-monitor "$OUTPUT"
