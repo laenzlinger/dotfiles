f0e13dfad3c2b55f6296eafa3f3b3eb02e5bc917
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2026-03-10 01:10:22 +0100

    nvim: fix lualine base16 warning, use tinted-nvim theme

--- dot_config/nvim/lua/plugins/no-time.lua
+++ dot_config/nvim/lua/plugins/no-time.lua
@@ -3,6 +3,7 @@ return {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     opts = function(_, opts)
+      opts.options.theme = require("lualine.themes.tinted")
       opts.sections.lualine_z = {}
     end,
   },
