17a0e6d733ae1257f68e3db9fd4c1ed6dd58b0c6
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2026-06-07 01:39:35 +0200

    fix keyboard backlight not following screen brightness

--- dot_config/sway/bin/executable_brightness.sh
+++ dot_config/sway/bin/executable_brightness.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 command -v brightnessctl >/dev/null || exit 1
 pct=$(brightnessctl -m | cut -d, -f4 | tr -d %)
-brightnessctl -d kbd_backlight set "${pct}%" -q 2>/dev/null || true
+brightnessctl --class=leds -d '*kbd_backlight*' set "${pct}%" -q 2>/dev/null || true
 WOBSOCK="${XDG_RUNTIME_DIR}/wob.sock"
 [[ -p "$WOBSOCK" ]] && echo "$pct BRIGHTNESS" > "$WOBSOCK"
