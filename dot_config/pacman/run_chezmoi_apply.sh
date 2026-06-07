32bb6ba5252f9eeed2164e9910005d54bb324154
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2026-06-07 23:36:13 +0200

    Restore run_chezmoi_apply.sh (fixes NAS backup run-before hook)

---
+++ dot_config/pacman/run_chezmoi_apply.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Update package lists before backup
+DIR=~/.local/share/chezmoi/dot_config/pacman/$(hostname)
+mkdir -p "$DIR"
+
+pacman -Qqen >"$DIR/pkglist.txt"
+pacman -Qqem >"$DIR/foreignpkglist.txt"
+echo "#optional dependencies" >"$DIR/optdeplist.txt"
+comm -13 <(pacman -Qqdt | sort) <(pacman -Qqdtt | sort) >>"$DIR/optdeplist.txt"
+
+# systemd services
+systemctl list-unit-files --state=enabled --no-legend | awk '{print $1}' >"$DIR/enabled_units.txt"
+systemctl --user list-unit-files --state=enabled --no-legend | awk '{print $1}' >"$DIR/enabled_user_units.txt"
