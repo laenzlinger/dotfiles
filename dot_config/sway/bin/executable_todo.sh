b31503977c4748f3d3963d273899dcf4f883704e
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2026-06-09 12:53:57 +0200

    fix: use valid foot window-size-chars (150x50)

--- dot_config/sway/bin/executable_todo.sh
+++ dot_config/sway/bin/executable_todo.sh
@@ -4,5 +4,5 @@ APP_ID="todo-nvim"
 if swaymsg -t get_tree | jq -e ".. | select(.app_id? == \"$APP_ID\" and .visible == true)" > /dev/null; then
     swaymsg "[app_id=\"$APP_ID\"] kill"
 else
-    foot --app-id="$APP_ID" --window-size-chars=150x0 nvim ~/notes/gtd/TODO.md
+    foot --app-id="$APP_ID" --window-size-chars=150x50 nvim ~/notes/gtd/TODO.md
 fi
