1247dd3e26a1d86b236da5e88d054124eaad9870
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2026-06-09 12:47:57 +0200

    feat: Mod+Shift+t toggles TODO.md in floating nvim

---
+++ dot_config/sway/bin/executable_todo.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+APP_ID="todo-nvim"
+if swaymsg -t get_tree | jq -e ".. | select(.app_id? == \"$APP_ID\" and .visible == true)" > /dev/null; then
+    swaymsg "[app_id=\"$APP_ID\"] kill"
+else
+    wezterm start --class "$APP_ID" -- nvim /notes/gtd/TODO.md
+fi
