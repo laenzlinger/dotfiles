a86ac66eece841cfbd4c5d1dc0ffcc6b0b2c8ef1
Christof Laenzlinger <6319634+laenzlinger@users.noreply.github.com>
2026-07-09 07:48:20 +0200

    feat: add Hammerspoon config with KeePassXC toggle (Cmd+Shift+S)
    Mirrors Sway Mod+Shift+S toggle pattern. Config stored in
    ~/.config/hammerspoon/ (non-default path via macOS defaults).
    Ignored on Linux.

---
+++ dot_config/hammerspoon/init.lua
@@ -0,0 +1,24 @@
+-- Toggle app: bring to front or hide if already focused
+local function toggleApp(appName)
+    local app = hs.application.find(appName)
+    if app then
+        if app:isFrontmost() then
+            app:hide()
+        else
+            app:activate()
+        end
+    else
+        hs.application.launchOrFocus(appName)
+    end
+end
+
+-- Keybindings (matching Sway patterns)
+-- Cmd+Shift+S: Toggle KeePassXC
+hs.hotkey.bind({"cmd", "shift"}, "s", function() toggleApp("KeePassXC") end)
+
+-- Reload config
+hs.hotkey.bind({"cmd", "shift"}, "r", function()
+    hs.reload()
+end)
+
+hs.alert.show("Hammerspoon loaded")
